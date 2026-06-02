533f17f1047cbdceef9436ceacf02b3837bab81d
ABDULLAH QIDWAI <abdullahqidwai92@gmail.com>
2026-06-02 08:45:01 +0530

    Update nodemailer transporter configuration

--- server/config/transporter.js
+++ server/config/transporter.js
@@ -1,11 +1,20 @@
 import nodemailer from "nodemailer"
 const transporter=nodemailer.createTransport({
-    service:"gmail",
-    
+    // service:"gmail",
+     host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
     auth:{
         user:process.env.USER,
         pass:process.env.PASS
 
     },
+});
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("SMTP Error:", error);
+  } else {
+    console.log("SMTP Server Ready");
+  }
 });
 export default transporter;
