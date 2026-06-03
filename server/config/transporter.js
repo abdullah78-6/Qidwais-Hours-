4a0a46d4bed24ec1c66b84582bf2a0ede6d4a018
ABDULLAH QIDWAI <abdullahqidwai92@gmail.com>
2026-06-03 14:55:20 +0530

    Update SMTP configuration for Brevo service

--- server/config/transporter.js
+++ server/config/transporter.js
@@ -1,20 +1,22 @@
 import nodemailer from "nodemailer"
 const transporter=nodemailer.createTransport({
-    // service:"gmail",
-     host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+    host:"smtp-relay.brevo.com",
+    
+    port:587,
+    secure:false,
+    
     auth:{
-        user:process.env.USER,
-        pass:process.env.PASS
+        user:process.env.BREVO_USER,
+        pass:process.env.BREVO_API_KEY
 
     },
 });
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("SMTP Error:", error);
-  } else {
-    console.log("SMTP Server Ready");
-  }
-});
+transporter.verify((error,success)=>{
+    if(error){
+        console.log("SMTP VERIFY ERROR",error);
+    }
+    else{
+        console.log("SMTP SERVER READY ");
+    }
+})
 export default transporter;
